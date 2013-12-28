c0e2a60d7aa2c1f081128af7f8fcf3eddee69de4
supernullset <sean.j.williamson@gmail.com>
2013-12-28 11:48:33 -0800

    minor test updates

--- test/lazy_test.jl
+++ test/lazy_test.jl
@@ -1,9 +1,17 @@
 using LazySequences
 
 # Cons
-c = cons(1, [0])
+c = cons(1, [42])
+# Test first(s::Cons)
 @assert first(c) == 1
-@assert first(rest(c)) == 0
+# Test rest(s::Cons)
+@assert first(rest(c)) == 42
+
+ct = cat([1], [42])
+# Test first(s::Cat)
+@assert first(ct) == 1
+# Test rest(s::Cat)
+
 
 # Test getindex implementation
 fibs = cat([0, 1], @lazyseq map(+, rest(fibs), fibs))
